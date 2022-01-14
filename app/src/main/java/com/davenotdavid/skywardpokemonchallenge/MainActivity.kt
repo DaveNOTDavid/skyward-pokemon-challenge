358239b63b879a9661405c3e3417c83a09c4b979
Dave Park <davepark37@gmail.com>
2022-01-14 13:42:07 -0500

    Nav component init

--- app/src/main/java/com/davenotdavid/skywardpokemonchallenge/MainActivity.kt
+++ app/src/main/java/com/davenotdavid/skywardpokemonchallenge/MainActivity.kt
@@ -1,26 +1,34 @@
 package com.davenotdavid.skywardpokemonchallenge
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.davenotdavid.skywardpokemonchallenge.ui.pokemoncards.PokemonCardsFragment
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.davenotdavid.skywardpokemonchallenge.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 /**
  * Marks an Android component class to be setup for injection with the standard
  * Hilt Dagger Android components.
- *
- * TODO: Set up nav components
  */
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, PokemonCardsFragment.newInstance())
-                .commitNow()
-        }
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        navController = findNavController(R.id.nav_host_fragment)
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
