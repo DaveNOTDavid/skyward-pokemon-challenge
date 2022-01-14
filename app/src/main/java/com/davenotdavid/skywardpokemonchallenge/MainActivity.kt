91a1956647ebaabb4de3cb0eccbd26c9763e49fb
Dave Park <davepark37@gmail.com>
2022-01-14 10:22:05 -0500

    Init commit

---
+++ app/src/main/java/com/davenotdavid/skywardpokemonchallenge/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.davenotdavid.skywardpokemonchallenge
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.davenotdavid.skywardpokemonchallenge.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
